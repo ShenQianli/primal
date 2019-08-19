6956611a8a6e69b3ebfa0867233f707224be7916
ShenQianli <shenqianli@pku.edu.cn>
2019-08-18 20:58:19 -0400

    update welcome information

--- python-package/pyprimal/__init__.py
+++ python-package/pyprimal/__init__.py
@@ -5,7 +5,7 @@ import os
 def test():
     """Show welcome information."""
     current_file = os.path.dirname(__file__)
-    print("Python package psm has been successfully imported!")
+    print("Python package primal has been successfully imported!")
     print("Version: "+open(os.path.join(current_file, './VERSION')).read().strip())
 
 __all__ = ["model"]
