a44fc925f87cf4f4e7e5ef148d2517a7e8ebce1f
zichongli5 <zichongli5@gmail.com>
2019-09-17 16:11:32 -0400

    update performance of dantzig selector in readme

--- profiling/benchmark.R
+++ profiling/benchmark.R
@@ -9,37 +9,24 @@ library(fastclime)
 
 #################### test dantzig selector time######################
 cat("**************Test dantzig selector time****************")
-
-trialN <- 2
-fastclime_time <- rep(0, 9)
-PRIMAL_time <- rep(0, 9)
-flare_time <- rep(0, 9)
-
-for (i in 1:9) {
-    for (j in 1:trialN) {
-        data <- dantzig.generator(n = 200, d = 100 * (i + 1), sparsity = 0.02)
-        fastclime_time[i] <- fastclime_time[i] + system.time(dantzig(data$X0, data$y, nlambda = 10000, lambda = 2 * 
-            sqrt(log(100 * (i + 1))/200)))
-        PRIMAL_time[i] <- PRIMAL_time[i] + system.time(ans <- Dantzig_solver(data$X0, data$y, max_it = 10000, lambda_threshold = 2 * 
-            sqrt(log(100 * (i + 1))/200)))
-        flare_time[i] <- flare_time[i] + system.time(xxx <- slim(data$X0, data$y, lambda = ans$lambda[ans$iterN]/200, 
-            method = "dantzig", verbose = FALSE))
-    }
-    
+fastclime_time <- rep(0, 6)
+PRIMAL_time <- rep(0, 6)
+for(i in 1:7){
+    data <- dantzig.generator(n = 200, d = 1000*i, sparsity = 0.03)
+    fastclime_time[i] <- system.time(dantzig(data$X0, data$y, nlambda = i*10000, lambda = 2 *
+    sqrt(log(i*1000)/200)))
+    ans <- Dantzig_solver(data$X0, data$y, max_it = i*10000, lambda_threshold = 2 *
+    sqrt(log(i*1000)/200))
+    PRIMAL_time[i] <-ans$runtime
 }
 
-# plot out the time result#
-time_data <- matrix(c(seq(200, 1000, 100), fastclime_time/trialN, PRIMAL_time/trialN, flare_time/trialN), ncol = 4)
+time_data <- matrix(c(seq(1000, 7000, 1000), fastclime_time, PRIMAL_time), ncol = 3)
 opar <- par(no.readonly = TRUE)
 par(family = "serif")
-plot(time_data[, 1:2], type = "l", col = "orange", lwd = 3, main = "Dantzig selector", xlab = "Dimension", ylab = "CPU Time(s)", 
-    ylim = c(0, max(flare_time/trialN) + 1), cex.main = 2, cex.lab = 1.5)
-legend("topleft", inset = 0.05, c("PRIMAL", "fastclime", "flare"), col = c("blue", "orange", "brown"), lwd = c(3, 3, 
-    3), cex = 1.5)
+plot(time_data[, 1:2], type = "l", col = "orange", lwd = 3, main = "Dantzig selector", xlab = "Dimension", ylab = "CPU Time(s)", cex.main = 2, cex.lab = 1.5)
+legend("topleft", inset = 0.05, c("PRIMAL", "fastclime"), col = c("blue", "orange"), lwd = c(3, 3), cex = 1.5)
 lines(time_data[, c(1, 3)], type = "l", lwd = 3, col = "blue")
-lines(time_data[, c(1, 4)], type = "l", lwd = 3, col = "brown")
 par(opar)
-
 
 
 #################### test compressed sensing time######################
@@ -53,7 +40,7 @@ test_fastclime_c <- function(X, y, lambda) {
     c_bar <- rep(0, 2 * ncol(X))
     b_bar <- rep(1, 2 * nrow(X))
     opt <- paralp(obj = c_obj, mat = A, rhs = b_rhs, obj_bar = c_bar, 
-                  rhs_bar = b_bar, lambda = 2 * sqrt(log(100 * (i + 1))/200))
+                  rhs_bar = b_bar, lambda = lambda)
     return(opt)
 }
 
@@ -64,7 +51,7 @@ flare_time2 <- rep(0, 9)
 for (i in 1:9) {
     for (j in 1:trialN) {
         data <- dantzig.generator(n = 200, d = 100 * (i + 1), sparsity = 0.1)
-        fastclime_time2[i] <- fastclime_time2[i] + system.time(test_fastclime_c(data$X0, data$y, lambda = 2 * sqrt(log(100 * 
+        fastclime_time2[i] <- fastclime_time2[i] + system.time(test_fastclime_c(data$X0, data$y, lambda = 2 * sqrt(log(100 *
             (i + 1))/200)))
         
         PRIMAL_time2[i] <- PRIMAL_time2[i] + system.time(ans <- CompressedSensing_solver(data$X0, data$y, max_it = 10000, 
@@ -89,9 +76,8 @@ par(opar)
 # plot out the time result together#
 opar <- par(no.readonly = TRUE)
 par(family = "serif", mfrow = c(1, 2))
-plot(time_data[, 1:2], type = "l", col = "orange", lwd = 3, main = "Dantzig selector", xlab = "Dimension", ylab = "CPU Time(s)", 
-    ylim = c(0, max(flare_time/trialN) + 1), cex.main = 2, cex.lab = 1.5)
-legend("topleft", inset = 0.05, c("PRIMAL", "fastclime", "flare"), col = c("blue", "orange", "brown"), lwd = c(3, 3, 
+plot(time_data[, 1:2], type = "l", col = "orange", lwd = 3, main = "Dantzig selector", xlab = "Dimension", ylab = "CPU Time(s)", cex.main = 2, cex.lab = 1.5)
+legend("topleft", inset = 0.05, c("PRIMAL", "fastclime"), col = c("blue", "orange"), lwd = c(3, 3,
     3), cex = 1.5)
 lines(time_data[, c(1, 3)], type = "l", lwd = 3, col = "blue")
 lines(time_data[, c(1, 4)], type = "l", lwd = 3, col = "brown")
